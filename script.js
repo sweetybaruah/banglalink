84877be34e06c08f048925593a840d880890429d
Sweety Barua <52146114+sweetybaruah@users.noreply.github.com>
2022-12-04 12:30:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,483 @@
+let data = [
+  {
+    DateUtc: "2022-11-29 15:00:00Z",
+    data: [
+      {
+        MatchNumber: 35,
+        RoundNumber: 3,
+        DateUtc: "2022-11-29 15:00:00Z",
+        Location: "Khalifa International Stadium",
+        HomeTeam: "Ecuador",
+        AwayTeam: "Senegal",
+        Group: "Group A",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+      {
+        MatchNumber: 36,
+        RoundNumber: 3,
+        DateUtc: "2022-11-29 15:00:00Z",
+        Location: "Al Bayt Stadium",
+        HomeTeam: "Netherlands",
+        AwayTeam: "Qatar",
+        Group: "Group A",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+    ],
+  },
+  {
+    DateUtc: "2022-11-29 19:00:00Z",
+    data: [
+      {
+        MatchNumber: 33,
+        RoundNumber: 3,
+        DateUtc: "2022-11-29 19:00:00Z",
+        Location: "Ahmad Bin Ali Stadium",
+        HomeTeam: "Wales",
+        AwayTeam: "England",
+        Group: "Group B",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+      {
+        MatchNumber: 34,
+        RoundNumber: 3,
+        DateUtc: "2022-11-29 19:00:00Z",
+        Location: "Al Thumama Stadium",
+        HomeTeam: "Iran",
+        AwayTeam: "USA",
+        Group: "Group B",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+    ],
+  },
+  {
+    DateUtc: "2022-11-30 15:00:00Z",
+    data: [
+      {
+        MatchNumber: 37,
+        RoundNumber: 3,
+        DateUtc: "2022-11-30 15:00:00Z",
+        Location: "Al Janoub Stadium",
+        HomeTeam: "Australia",
+        AwayTeam: "Denmark",
+        Group: "Group D",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+      {
+        MatchNumber: 38,
+        RoundNumber: 3,
+        DateUtc: "2022-11-30 15:00:00Z",
+        Location: "Education City Stadium",
+        HomeTeam: "Tunisia",
+        AwayTeam: "France",
+        Group: "Group D",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+    ],
+  },
+  {
+    DateUtc: "2022-11-30 19:00:00Z",
+    data: [
+      {
+        MatchNumber: 39,
+        RoundNumber: 3,
+        DateUtc: "2022-11-30 19:00:00Z",
+        Location: "Stadium 974",
+        HomeTeam: "Poland",
+        AwayTeam: "Argentina",
+        Group: "Group C",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+      {
+        MatchNumber: 40,
+        RoundNumber: 3,
+        DateUtc: "2022-11-30 19:00:00Z",
+        Location: "Lusail Stadium",
+        HomeTeam: "SaudiArabia",
+        AwayTeam: "Mexico",
+        Group: "Group C",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+    ],
+  },
+  {
+    DateUtc: "2022-12-01 15:00:00Z",
+    data: [
+      {
+        MatchNumber: 41,
+        RoundNumber: 3,
+        DateUtc: "2022-12-01 15:00:00Z",
+        Location: "Ahmad Bin Ali Stadium",
+        HomeTeam: "Croatia",
+        AwayTeam: "Belgium",
+        Group: "Group F",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+      {
+        MatchNumber: 42,
+        RoundNumber: 3,
+        DateUtc: "2022-12-01 15:00:00Z",
+        Location: "Al Thumama Stadium",
+        HomeTeam: "Canada",
+        AwayTeam: "Morocco",
+        Group: "Group F",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+    ],
+  },
+  {
+    DateUtc: "2022-12-01 19:00:00Z",
+    data: [
+      {
+        MatchNumber: 43,
+        RoundNumber: 3,
+        DateUtc: "2022-12-01 19:00:00Z",
+        Location: "Khalifa International Stadium",
+        HomeTeam: "Japan",
+        AwayTeam: "Spain",
+        Group: "Group E",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+      {
+        MatchNumber: 44,
+        RoundNumber: 3,
+        DateUtc: "2022-12-01 19:00:00Z",
+        Location: "Al Bayt Stadium",
+        HomeTeam: "CostaRica",
+        AwayTeam: "Germany",
+        Group: "Group E",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+    ],
+  },
+  {
+    DateUtc: "2022-12-02 15:00:00Z",
+    data: [
+      {
+        MatchNumber: 45,
+        RoundNumber: 3,
+        DateUtc: "2022-12-02 15:00:00Z",
+        Location: "Al Janoub Stadium",
+        HomeTeam: "Ghana",
+        AwayTeam: "Uruguay",
+        Group: "Group H",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+      {
+        MatchNumber: 46,
+        RoundNumber: 3,
+        DateUtc: "2022-12-02 15:00:00Z",
+        Location: "Education City Stadium",
+        HomeTeam: "Korea",
+        AwayTeam: "Portugal",
+        Group: "Group H",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+    ],
+  },
+  {
+    DateUtc: "2022-12-02 19:00:00Z",
+    data: [
+      {
+        MatchNumber: 47,
+        RoundNumber: 3,
+        DateUtc: "2022-12-02 19:00:00Z",
+        Location: "Stadium 974",
+        HomeTeam: "Serbia",
+        AwayTeam: "Switzerland",
+        Group: "Group G",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+      {
+        MatchNumber: 48,
+        RoundNumber: 3,
+        DateUtc: "2022-12-02 19:00:00Z",
+        Location: "Lusail Stadium",
+        HomeTeam: "Cameroon",
+        AwayTeam: "Brazil",
+        Group: "Group G",
+        HomeTeamScore: null,
+        AwayTeamScore: null,
+      },
+    ],
+  },
+
+  // Round 16 matches
+  // Round 16 matches
+  // Round 16 matches
+
+  {
+    MatchNumber: 49,
+    RoundNumber: 4,
+    DateUtc: "2022-12-03 15:00:00Z",
+    Location: "TBA",
+    HomeTeam: "Netherlands",
+    AwayTeam: "USA",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 50,
+    RoundNumber: 4,
+    DateUtc: "2022-12-03 19:00:00Z",
+    Location: "TBA",
+    HomeTeam: "Argentina",
+    AwayTeam: "Australia",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 52,
+    RoundNumber: 4,
+    DateUtc: "2022-12-04 15:00:00Z",
+    Location: "TBA",
+    HomeTeam: "France",
+    AwayTeam: "Poland",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 51,
+    RoundNumber: 4,
+    DateUtc: "2022-12-04 19:00:00Z",
+    Location: "TBA",
+    HomeTeam: "England",
+    AwayTeam: "Senegal",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 53,
+    RoundNumber: 4,
+    DateUtc: "2022-12-05 15:00:00Z",
+    Location: "TBA",
+    HomeTeam: "Japan",
+    AwayTeam: "Croatia",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 54,
+    RoundNumber: 4,
+    DateUtc: "2022-12-05 19:00:00Z",
+    Location: "TBA",
+    HomeTeam: "Brazil",
+    AwayTeam: "Korea",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 55,
+    RoundNumber: 4,
+    DateUtc: "2022-12-06 15:00:00Z",
+    Location: "TBA",
+    HomeTeam: "Morocco",
+    AwayTeam: "Spain",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 56,
+    RoundNumber: 4,
+    DateUtc: "2022-12-06 19:00:00Z",
+    Location: "TBA",
+    HomeTeam: "Portugal",
+    AwayTeam: "Switzerland",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 58,
+    RoundNumber: 5,
+    DateUtc: "2022-12-09 15:00:00Z",
+    Location: "TBA",
+    HomeTeam: "To be announced",
+    AwayTeam: "To be announced",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 57,
+    RoundNumber: 5,
+    DateUtc: "2022-12-09 19:00:00Z",
+    Location: "TBA",
+    HomeTeam: "To be announced",
+    AwayTeam: "To be announced",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 60,
+    RoundNumber: 5,
+    DateUtc: "2022-12-10 15:00:00Z",
+    Location: "TBA",
+    HomeTeam: "To be announced",
+    AwayTeam: "To be announced",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 59,
+    RoundNumber: 5,
+    DateUtc: "2022-12-10 19:00:00Z",
+    Location: "TBA",
+    HomeTeam: "To be announced",
+    AwayTeam: "To be announced",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 61,
+    RoundNumber: 6,
+    DateUtc: "2022-12-13 19:00:00Z",
+    Location: "TBA",
+    HomeTeam: "To be announced",
+    AwayTeam: "To be announced",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 62,
+    RoundNumber: 6,
+    DateUtc: "2022-12-14 19:00:00Z",
+    Location: "TBA",
+    HomeTeam: "To be announced",
+    AwayTeam: "To be announced",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 63,
+    RoundNumber: 7,
+    DateUtc: "2022-12-17 15:00:00Z",
+    Location: "TBA",
+    HomeTeam: "To be announced",
+    AwayTeam: "To be announced",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+  {
+    MatchNumber: 64,
+    RoundNumber: 7,
+    DateUtc: "2022-12-18 15:00:00Z",
+    Location: "TBA",
+    HomeTeam: "To be announced",
+    AwayTeam: "To be announced",
+    Group: null,
+    HomeTeamScore: null,
+    AwayTeamScore: null,
+  },
+];
+
+  var country1= document.querySelector('.country1')
+  var country2= document.querySelector('.country2')
+  var flag1= document.querySelector('.flag1')
+  var flag2= document.querySelector('.flag2')
+  var hour = document.querySelector('.hour')
+  var min = document.querySelector('.min')
+  var sec = document.querySelector('.sec')
+  //match current date with the array list data
+var selected, i;
+showtime();
+function showtime() {
+  for (i = 0; i < data.length; i++) {
+    var current = new Date();
+    var check = new Date(data[i].DateUtc);
+    if (check > current) {
+      selected = data[i];
+      break;
+    }
+  }
+}
+
+function randomTeam(data) {
+  var i = Math.random() > 0.49 ? 1 : 0;
+  // console.log(i)
+  var rselected = data["data"][i];
+  country1.innerText = rselected.HomeTeam;
+  country2.innerText = rselected.AwayTeam;
+  flag1.style.backgroundImage = `url(300x250/FlagPNGs/${selected.HomeTeam}.png)`;
+  flag2.style.backgroundImage = `url(300x250/FlagPNGs/${selected.AwayTeam}.png)`;
+}
+
+function handleTickInit(tick) {
+  var counter = Tick.count.down(new Date(selected.DateUtc), {
+    format: ["h", "m", "s"],
+  });
+  if (!!selected["data"]) {
+    randomTeam(selected);
+  } else {
+    country1.innerText = selected.HomeTeam;
+    country2.innerText = selected.AwayTeam;
+    flag1.style.backgroundImage = `url(300x250/FlagPNGs/${selected.HomeTeam}.png)`;
+    flag2.style.backgroundImage = `url(300x250/FlagPNGs/${selected.AwayTeam}.png)`;
+  }
+
+  if (data.stat == false) {
+    stat.innerText = "";
+  } else {
+    let time = new Date(selected.DateUtc);
+    stat.innerText = time.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+  }
+
+  counter.onupdate = function (value) {
+    tick.value = value;
+  };
+
+  counter.onended = function () {
+    showtime();
+    handleTickInit(tick);
+  };
+  function updateWCTime() {
+    var kickoff = new Date(selected.DateUtc);
+    var diff = kickoff - new Date();
+    var dd = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var hh = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var mm = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    var ss = Math.floor((diff % (1000 * 60)) / 1000);
+
+    var hhh = hh + dd * 24;
+    var h = hhh < 10 ? "0" + hhh : hhh;
+    var m = mm < 10 ? "0" + mm : mm;
+    var s = ss < 10 ? "0" + ss : ss;
+
+    hour.innerHTML = h;
+    min.innerHTML = m;
+    sec.innerHTML = s;
+    setTimeout(updateWCTime, 1000);
+  }
+  setTimeout(updateWCTime, false);
+}
